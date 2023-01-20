8902693d4d1b7edd56c66a69805041e80505d13d
fabriziocl <f.castrolopez@hotmail.com>
2023-01-20 03:55:48 -0300

    api index updated

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config()
-const {PORT} = process.env
+const { PORT } = process.env
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
