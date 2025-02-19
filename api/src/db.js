d590c35b336f7bb5c6e774dd09724231382217de
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2025-02-19 01:01:14 -0300

    fix db connection issue

--- api/src/db.js
+++ api/src/db.js
@@ -1,3 +1,53 @@
+// require('dotenv').config();
+// const { Sequelize } = require('sequelize');
+// const fs = require('fs');
+// const path = require('path');
+// const {
+//   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+// } = process.env;
+
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   // dialectOptions: {
+//   //   ssl: {
+//   //     require: true,
+//   //     rejectUnauthorized: false // You may need to set this option to true if you're using a self-signed certificate
+//   //   }
+//   // }
+// });
+// const basename = path.basename(__filename);
+
+// const modelDefiners = [];
+
+// // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+// fs.readdirSync(path.join(__dirname, '/models'))
+//   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+//   .forEach((file) => {
+//     modelDefiners.push(require(path.join(__dirname, '/models', file)));
+//   });
+
+// // Injectamos la conexion (sequelize) a todos los modelos
+// modelDefiners.forEach(model => model(sequelize));
+// // Capitalizamos los nombres de los modelos ie: product => Product
+// let entries = Object.entries(sequelize.models);
+// let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+// sequelize.models = Object.fromEntries(capsEntries);
+
+// // En sequelize.models están todos los modelos importados como propiedades
+// // Para relacionarlos hacemos un destructuring
+// const { Dog, Temperament } = sequelize.models;
+
+// // Aca vendrian las relaciones
+// // Product.hasMany(Reviews);
+// Dog.belongsToMany(Temperament, {through: "DogXTemperament"})
+// Temperament.belongsToMany(Dog, {through: "DogXTemperament"})
+
+// module.exports = {
+//   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+//   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+// };
+
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
@@ -6,18 +56,27 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false // You may need to set this option to true if you're using a self-signed certificate
-  //   }
-  // }
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: false
+  }
 });
-const basename = path.basename(__filename);
 
+// Verificamos si la conexión se establece correctamente
+const testDbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+testDbConnection();
+
+const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
@@ -29,6 +88,7 @@ fs.readdirSync(path.join(__dirname, '/models'))
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize));
+
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
@@ -38,12 +98,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 // Para relacionarlos hacemos un destructuring
 const { Dog, Temperament } = sequelize.models;
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
-Dog.belongsToMany(Temperament, {through: "DogXTemperament"})
-Temperament.belongsToMany(Dog, {through: "DogXTemperament"})
+// Relaciones entre modelos
+Dog.belongsToMany(Temperament, {through: "DogXTemperament"});
+Temperament.belongsToMany(Dog, {through: "DogXTemperament"});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize,     // para importar la conexión { conn } = require('./db.js');
 };
