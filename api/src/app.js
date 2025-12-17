d9a884565abe909cd90cd4c47675866736c98a25
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2025-12-17 19:49:08 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -38,9 +38,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const routes = require('./routes/index.js'); // Asegúrate de tener las rutas bien configuradas
+const routes = require('./routes/index.js'); 
 
-require('./db.js'); // Conexión a la base de datos
+// require('./db.js'); 
 
 const server = express();
 server.name = 'API';
