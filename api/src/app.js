38e42e1dbaf0b58832824d5af0cd7bdd04622187
Fabrizio Castro López <75953508+fabriziocl@users.noreply.github.com>
2025-12-17 21:13:13 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -36,51 +36,42 @@
 
 
 
-// const express = require('express');
-// const cookieParser = require('cookie-parser');
-// const bodyParser = require('body-parser');
-// const morgan = require('morgan');
-// const routes = require('./routes/index.js'); 
-
-// // require('./db.js'); 
-
-// const server = express();
-// server.name = 'API';
-
-// // Middleware y configuración
-// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-// server.use(bodyParser.json({ limit: '50mb' }));
-// server.use(cookieParser());
-// server.use(morgan('dev'));
-
-// // Configuración CORS
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); 
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
-
-// // Rutas
-// server.use('/', routes);
-
-// // Manejo de errores
-// server.use((err, req, res, next) => { 
-//   const status = err.status || 500;
-//   const message = err.message || err;
-//   console.error(err);
-//   res.status(status).send(message);
-// });
-
-// module.exports = server;
-
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const routes = require('./routes/index.js'); 
+
+// require('./db.js'); 
 
 const server = express();
+server.name = 'API';
 
-server.get('/ping', (req, res) => {
-  res.json({ ok: true });
+// Middleware y configuración
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+
+// Configuración CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+// Rutas
+server.use('/', routes);
+
+// Manejo de errores
+server.use((err, req, res, next) => { 
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
 });
 
 module.exports = server;
+
